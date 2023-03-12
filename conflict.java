82987e028fd4258fd1921a1d977608a425dc6c76
Pial Rafsan <pial1907017@stud.kuet.ac.bd>
2023-03-12 10:49:14 +0600

    master commited

--- conflict.java
+++ conflict.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class conflict {
+public class conflict {
+    public int x;
+    public int y;
+
+    public conflict(int x, int y) {
+        this.x = x;
+        this.y = y;
+        System.out.println("master");
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
