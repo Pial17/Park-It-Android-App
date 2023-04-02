d154595849ccec7ef08f02dedf8fdfc0438388a5
Pial Rafsan <pial1907017@stud.kuet.ac.bd>
2023-04-02 22:08:51 +0600

    getter, setter and constructors added.

---
+++ app/src/androidTest/java/lab1.java
@@ -0,0 +1,43 @@
+public class lab1 {
+    int a,b;
+    String d,e;
+
+    public lab1(int a, int b, String d, String e) {
+        this.a = a;
+        this.b = b;
+        this.d = d;
+        this.e = e;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public void setE(String e) {
+        this.e = e;
+    }
+}
