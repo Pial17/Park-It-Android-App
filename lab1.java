34b31f23ee2733ebbb4c572d756f876684598d0c
Pial Rafsan <pial1907017@stud.kuet.ac.bd>
2023-04-03 16:54:20 +0600

    master commited

---
+++ lab1.java
@@ -0,0 +1,43 @@
+public class lab1 {
+    int a,b,p;
+    String x,y,pial;
+
+    public lab1(int a, int b, String x, String y) {
+        this.a = a;
+        this.b = b;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+}
