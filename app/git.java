6866903eec71ee167feba414480cbfbae76b3ed4
Pial Rafsan <pial1907017@stud.kuet.ac.bd>
2023-04-03 17:25:41 +0600

    git commited

---
+++ app/git.java
@@ -0,0 +1,40 @@
+public class git {
+    int a,b;
+    String x,y;
+
+    public git(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+}
